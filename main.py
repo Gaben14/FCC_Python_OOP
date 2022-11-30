bc146b3d8b0cc23e94ecb43320dbceba1e8dd969
Gabor Zorad EPAM <108816298+gabor-zorad-epam@users.noreply.github.com>
2022-11-30 14:58:31 +0100

    Defining the constructor and instance attributes.

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 class Item:
-    def calculate_total_price(self, x, y):
-        return x * y
+    #Constructor creation
+    def __init__(self, name: str, price: int, quantity: int):
+        # Assert validation - Run validations to the received arguments
+        assert price >= 0, f"Price {price} should be greater than or equal to 0 (zero)"
+        assert quantity >= 0, f"Quantity {quantity} should be greater than or equal to 0 (zero)"
 
+        # Assign to self object
+        # The self.name will be equal to the name value of each new instance.
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 if __name__ == '__main__':
     print('PyCharm')
 
-    item1 = Item() # Creating a new instance of the Item class
-    item1.name = "Phone"
-    item1.price = 100
-    item1.quantity = 5
-    print(item1.calculate_total_price(item1.price,item1.quantity))
-
-    item2 = Item()  # Creating a new instance of the Item class
-    item2.name = "Laptop"
-    item2.price = 1000
-    item2.quantity = 3
-    print(item2.calculate_total_price(item2.price, item2.quantity))
+    item1 = Item("Phone", 100, -1) # Creating a new instance of the Item class
+    item2 = Item("Laptop", 1000, 3)  # Creating a new instance of the Item class
+    #Calling the calculate_total_price() method for both instances.
+    print(item1.calculate_total_price())
+    print(item2.calculate_total_price())
