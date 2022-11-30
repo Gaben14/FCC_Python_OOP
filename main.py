33ebc68767053b22538abeb235f895de15574c49
Gabor Zorad EPAM <108816298+gabor-zorad-epam@users.noreply.github.com>
2022-11-30 17:13:13 +0100

    Comparing instance and class level attributes.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 class Item:
+    pay_rate = 0.8 # Class Attribute
     #Constructor creation
     def __init__(self, name: str, price: int, quantity: int):
         # Assert validation - Run validations to the received arguments
@@ -14,11 +15,27 @@ class Item:
     def calculate_total_price(self):
         return self.price * self.quantity
 
+    def apply_discount(self):
+        self.price = self.price * Item.pay_rate
+    # We need to use the Instance class, if the instance does not have the specific attribute
+    # it will look up for the class level attribute
+
 if __name__ == '__main__':
     print('PyCharm')
 
-    item1 = Item("Phone", 100, -1) # Creating a new instance of the Item class
-    item2 = Item("Laptop", 1000, 3)  # Creating a new instance of the Item class
+    item1 = Item("Phone", 100, 1) # Creating a new instance of the Item class
+    item2 = Item("Laptop", 100, 1)  # Creating a new instance of the Item class
     #Calling the calculate_total_price() method for both instances.
-    print(item1.calculate_total_price())
-    print(item2.calculate_total_price())
+    #print(item1.calculate_total_price())
+    #print(item2.calculate_total_price())
+
+    #print(Item.pay_rate) # Accessing the Class Attribute
+    #print(f"item1 {item1.pay_rate} ")
+    #print(f"item2 {item2.pay_rate} ")
+
+    item1.apply_discount()
+    print(f"item1.price {item1.price}") #Class Level attribute
+
+    item2.pay_rate = 0.7 #Instance level attribute creation
+    item2.apply_discount()
+    print(f"item2.price {item2.price}")
