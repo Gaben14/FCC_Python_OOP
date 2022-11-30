11196479d264d0366ef8df4a833fbfb03056de93
Gabor Zorad EPAM <108816298+gabor-zorad-epam@users.noreply.github.com>
2022-11-30 17:55:14 +0100

    Creating the Class Method

--- main.py
+++ main.py
@@ -24,6 +24,10 @@ class Item:
     # We need to use the Instance class, if the instance does not have the specific attribute
     # it will look up for the class level attribute
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        print("Class Method")
+
     # With the __repr__ we can specify the way we want to represent our object, when printing it out
     def __repr__(self):
         return f"Item('{self.name}', {self.price}, {self.quantity})"
@@ -31,28 +35,13 @@ class Item:
 if __name__ == '__main__':
     print('PyCharm')
 
-    #item1 = Item("Phone", 100, 1) # Creating a new instance of the Item class
-    #item2 = Item("Laptop", 100, 1)  # Creating a new instance of the Item class
-    #Calling the calculate_total_price() method for both instances.
-    #print(item1.calculate_total_price())
-    #print(item2.calculate_total_price())
-
-    #print(Item.pay_rate) # Accessing the Class Attribute
-    #print(f"item1 {item1.pay_rate} ")
-    #print(f"item2 {item2.pay_rate} ")
-
-    #item1.apply_discount()
-    #print(f"item1.price {item1.price}") #Class Level attribute
-
-    #item2.pay_rate = 0.7 #Instance level attribute creation
-    #item2.apply_discount()
-    #print(f"item2.price {item2.price}")
-
     item1 = Item("Phone", 100, 1)
     item2 = Item("Laptop", 1000, 3)
     item3 = Item("Cable", 10, 5)
     item4 = Item("Mouse", 50, 5)
     item5 = Item("Keyboard", 75, 5)
 
-    print(Item.all)
+    #print(Item.all)
+
+    print(Item.instantiate_from_csv())
 
