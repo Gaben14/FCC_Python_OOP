ffddcb23ca4aa41eb264d066b10819cb06d39a4f
Gabor Zorad EPAM <108816298+gabor-zorad-epam@users.noreply.github.com>
2022-11-30 17:33:32 +0100

    Creating list to store all instances

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 class Item:
     pay_rate = 0.8 # Class Attribute
+    all = [] # List to store all the instances that have been created
     #Constructor creation
     def __init__(self, name: str, price: int, quantity: int):
         # Assert validation - Run validations to the received arguments
@@ -12,6 +13,9 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+        # Actions to execute
+        Item.all.append(self) # self is the instance itself
+
     def calculate_total_price(self):
         return self.price * self.quantity
 
@@ -20,11 +24,15 @@ class Item:
     # We need to use the Instance class, if the instance does not have the specific attribute
     # it will look up for the class level attribute
 
+    # With the __repr__ we can specify the way we want to represent our object, when printing it out
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
+
 if __name__ == '__main__':
     print('PyCharm')
 
-    item1 = Item("Phone", 100, 1) # Creating a new instance of the Item class
-    item2 = Item("Laptop", 100, 1)  # Creating a new instance of the Item class
+    #item1 = Item("Phone", 100, 1) # Creating a new instance of the Item class
+    #item2 = Item("Laptop", 100, 1)  # Creating a new instance of the Item class
     #Calling the calculate_total_price() method for both instances.
     #print(item1.calculate_total_price())
     #print(item2.calculate_total_price())
@@ -33,9 +41,18 @@ if __name__ == '__main__':
     #print(f"item1 {item1.pay_rate} ")
     #print(f"item2 {item2.pay_rate} ")
 
-    item1.apply_discount()
-    print(f"item1.price {item1.price}") #Class Level attribute
+    #item1.apply_discount()
+    #print(f"item1.price {item1.price}") #Class Level attribute
 
-    item2.pay_rate = 0.7 #Instance level attribute creation
-    item2.apply_discount()
-    print(f"item2.price {item2.price}")
+    #item2.pay_rate = 0.7 #Instance level attribute creation
+    #item2.apply_discount()
+    #print(f"item2.price {item2.price}")
+
+    item1 = Item("Phone", 100, 1)
+    item2 = Item("Laptop", 1000, 3)
+    item3 = Item("Cable", 10, 5)
+    item4 = Item("Mouse", 50, 5)
+    item5 = Item("Keyboard", 75, 5)
+
+    print(Item.all)
+
